9770b7aa01d8ff0fd8d07b5359fd14e425fe3f81
Ana María <anahernandez814@gmail.com>
2021-05-29 16:00:43 -0500

    Se crea la instancia de televisión

--- main.js
+++ main.js
@@ -22,15 +22,11 @@ console.log(tv);
 //Escribir (\n->salto de linea)
 //process.stdout.write("Hola mundo");
 //Obtener datos del usuario
-let nombre = "";
-console.log(nombre);
 
 
-let nevera_pd = ['¿Cual es el tipo consumo?', '¿Cual es la procedencia?', '¿Ingrese el volumen?'];
+let nevera_pd = ['¿Cual es el tipo consumo?: ', '¿Cual es la procedencia?: ', '¿Ingrese el volumen?: '];
 let nevera_pr = [];
 
-
-
 function instanciaNevera() {
     function neverapd(i) {
         process.stdout.write(nevera_pd[i]);
@@ -53,29 +49,30 @@ function instanciaNevera() {
     neverapd(0);
 }
 
-instanciaNevera();
+let television_pd = ['¿Cual es el tipo consumo?: ', '¿Cual es la procedencia?: ', '¿Cual es su tamaño?: ', '¿Tiene sintonizador?: '];
+let television_pr = [];
 
-
-function instanciaNevera() {
-    function neverapd(i) {
-        process.stdout.write(nevera_pd[i]);
+function instanciaTelevisor() {
+    function televisorpd(i) {
+        process.stdout.write(television_pd[i]);
     }
 
     process.stdin.on('data', function(data) {
-        nevera_pr.push(data.toString().trim());
+        television_pr.push(data.toString().trim());
 
-        if (nevera_pr.length < nevera_pd.length) {
-            neverapd(nevera_pr.length)
+        if (television_pr.length < television_pd.length) {
+            televisorpd(television_pr.length)
         } else {
-            console.log(nevera_pr);
-            let nev = new Nevera(nevera_pr[0], nevera_pr[1], parseInt(nevera_pr[2]));
-            nev.calcularPrecio();
-            console.log(nev.precio);
+            console.log(television_pr);
+            let tev = new Televisor(television_pr[0], television_pr[1], parseInt(television_pr[2]),television_pr[3]);
+            tev.calcularPrecio();
+            console.log(tev.precio);
             process.exit();
         }
 
     });
-    neverapd(0);
+    televisorpd(0);
 }
 
-instanciaNevera();
+//instanciaNevera();
+instanciaTelevisor();
