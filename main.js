3d21182a0765a395704ea24f3b8ebba69c5d31bd
Ana María <anahernandez814@gmail.com>
2021-05-29 15:34:17 -0500

    Se realiza adición de lainstancia nevera nuevamente, commit de prueba

--- main.js
+++ main.js
@@ -18,13 +18,6 @@ console.log(tv.consumo, tv.procedencia);
 console.log(tv);
 */
 
-//Prueba Nevera
-/*
-let nev = new Nevera('C', 'internacional', 130)
-nev.calcularPrecio();
-console.log(nev.precio);
-*/
-
 //Prueba de ingreso y guardar datos
 //Escribir (\n->salto de linea)
 //process.stdout.write("Hola mundo");
@@ -36,6 +29,31 @@ console.log(nombre);
 let nevera_pd = ['¿Cual es el tipo consumo?', '¿Cual es la procedencia?', '¿Ingrese el volumen?'];
 let nevera_pr = [];
 
+
+
+function instanciaNevera() {
+    function neverapd(i) {
+        process.stdout.write(nevera_pd[i]);
+    }
+
+    process.stdin.on('data', function(data) {
+        nevera_pr.push(data.toString().trim());
+
+        if (nevera_pr.length < nevera_pd.length) {
+            neverapd(nevera_pr.length)
+        } else {
+            console.log(nevera_pr);
+            let nev = new Nevera(nevera_pr[0], nevera_pr[1], parseInt(nevera_pr[2]));
+            nev.calcularPrecio();
+            console.log(nev.precio);
+            process.exit();
+        }
+
+    });
+    neverapd(0);
+}
+
+instanciaNevera();
 
 
 function instanciaNevera() {
