5642553c0139aea2b42a80c79337a88e452f9da7
Cristian <camilo.santamaria322@gmail.com>
2021-05-29 13:23:28 -0500

    Se agrega README al proyecto

---
+++ README.md
@@ -0,0 +1,2 @@
+# Trabajo Pair Programing | Javascript POO
+Este trabajo fue realizado por Ana maria Hernandez y Cristian Camilo Quiñones
--- main.js
+++ main.js
@@ -5,6 +5,4 @@ const elec = new Electrodomestico('A', 'Nacional');
 console.log(elec.prueba());
 */
 
-import { square, diag } from 'Electrodomestico';
-console.log(square(11)); // 121
-console.log(diag(4, 3)); // 5
+console.log(objeto);
