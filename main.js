3461595f7ac686106094166abb1a2ad52c8cc212
Cristian <camilo.santamaria322@gmail.com>
2021-05-29 16:46:36 -0500

    Se agrega instancia de electrodomesticos y se empieza a definir el ciclo para los arreglos del inventario.

--- main.js
+++ main.js
@@ -2,30 +2,44 @@ const Electrodomestico = require("./Electrodomestico");
 const Televisor = require("./Televisor");
 const Nevera = require("./Nevera");
 
-
-/* Prueba Electrodomestico
-const elec = new Electrodomestico('A', 'Nacional');
-console.log(elec.prueba());
-*/
-
-//Prueba televisor
-/*
-let tv = new Televisor('A', 'nacional', 100, "no");
-tv.calcularPrecioPulgadas();
-tv.calcularPrecio();
-console.log(tv.precio);
-console.log(tv.consumo, tv.procedencia);
-console.log(tv);
-*/
-
-//Prueba de ingreso y guardar datos
-//Escribir (\n->salto de linea)
-//process.stdout.write("Hola mundo");
-//Obtener datos del usuario
-
-
+//Variables para instancias
+let electrodomestico_pd = ['¿Cual es el tipo consumo?: ', '¿Cual es la procedencia?:'];
+let electrodomestico_pr = [];
 let nevera_pd = ['¿Cual es el tipo consumo?: ', '¿Cual es la procedencia?: ', '¿Ingrese el volumen?: '];
 let nevera_pr = [];
+let television_pd = ['¿Cual es el tipo consumo?: ', '¿Cual es la procedencia?: ', '¿Cual es su tamaño?: ', '¿Tiene sintonizador?: '];
+let television_pr = [];
+
+//Arreglos para inventario
+let arregloElectros = [];
+let arregloNeveras = [];
+let arreglotvs = [];
+
+let sw;
+while (sw === "terminar") {
+
+}
+
+function instanciaElectrodomestico() {
+    function electrodomesticopd(i) {
+        process.stdout.write(electrodomestico_pd[i]);
+    }
+
+    process.stdin.on('data', function(data) {
+        electrodomestico_pr.push(data.toString().trim());
+
+        if (electrodomestico_pr.length < electrodomestico_pd.length) {
+            electrodomesticopd(electrodomestico_pr.length)
+        } else {
+            //console.log(electrodomestico_pr);
+            let elect = new Electrodomestico(electrodomestico_pr[0], electrodomestico_pr[1]);
+            console.log(elect);
+            process.exit();
+        }
+
+    });
+    electrodomesticopd(0);
+}
 
 function instanciaNevera() {
     function neverapd(i) {
@@ -49,9 +63,6 @@ function instanciaNevera() {
     neverapd(0);
 }
 
-let television_pd = ['¿Cual es el tipo consumo?: ', '¿Cual es la procedencia?: ', '¿Cual es su tamaño?: ', '¿Tiene sintonizador?: '];
-let television_pr = [];
-
 function instanciaTelevisor() {
     function televisorpd(i) {
         process.stdout.write(television_pd[i]);
@@ -63,10 +74,12 @@ function instanciaTelevisor() {
         if (television_pr.length < television_pd.length) {
             televisorpd(television_pr.length)
         } else {
-            console.log(television_pr);
-            let tev = new Televisor(television_pr[0], television_pr[1], parseInt(television_pr[2]),television_pr[3]);
-            tev.calcularPrecio();
-            console.log(tev.precio);
+            //console.log(television_pr);
+            let tev = new Televisor(television_pr[0], television_pr[1], parseInt(television_pr[2]), television_pr[3]);
+            let tev2 = new Televisor('B', 'internacional', parseInt(television_pr[2]), television_pr[3]);
+            arreglotv.push(tev);
+            arreglotv.push(tev2);
+            console.log(arreglotvs);
             process.exit();
         }
 
@@ -74,5 +87,7 @@ function instanciaTelevisor() {
     televisorpd(0);
 }
 
+// <----Llamado de instancias y pruebas--->
 //instanciaNevera();
-instanciaTelevisor();
+//instanciaTelevisor();
+//instanciaElectrodomestico();
